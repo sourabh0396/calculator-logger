8098438620c2506d0dd31694af2011adb8eda4be
Sourabh Patil <95458021+sourabh0396@users.noreply.github.com>
2024-09-25 22:02:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,38 +12,10 @@ function App() {
       <Calculator/>
     </div>
     <div>
-      {/* <DummyCalculatorTable/> */}
+      <DummyCalculatorTable/>
     </div>
     </>
   );
 }
 
 export default App;
-// import logo from './logo.svg';
-
-// import './App.css';
-// import Calculator from './Calculator';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Calculator/>
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-//     </div>
-//   );
-// }
-
-// export default App;
